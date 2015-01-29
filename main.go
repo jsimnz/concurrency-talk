d0a33a8e7dd0560204f358dec4e6db1e7ac93d9e
John-Alan Simmons <johnalan.simmons@polylabs.ca>
2015-01-29 14:44:52 -0500

    Created an N chained channel

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+const (
+	NUM_CHANNELS = 2
+)
+
+func connectN(chans ...chan string) {
+	for i, ch := range chans {
+		if i != len(chans)-1 {
+			go connect(chans[i+1], ch)
+		}
+	}
+}
+
 func connect(out chan string, in chan string) {
 	for str := range in {
 		out <- str
@@ -11,10 +23,12 @@ func connect(out chan string, in chan string) {
 }
 
 func main() {
-	in := make(chan string)
-	out := make(chan string)
-	go connect(out, in)
+	chans := make([]chan string, NUM_CHANNELS)
+	for i := 0; i < NUM_CHANNELS; i++ {
+		chans[i] = make(chan string)
+	}
+	connectN(chans...)
 
-	in <- "Hello World"
-	fmt.Println(<-out)
+	chans[0] <- "Hello World"            // in at the beggining of the chain
+	fmt.Println(<-chans[NUM_CHANNELS-1]) // out at the end
 }
