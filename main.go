5b314964206ee311c022755e3375e4caac478ade
John-Alan Simmons <johnalan.simmons@polylabs.ca>
2015-01-29 15:58:38 -0500

    Created sync example of workers

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 const (
-	NUM_WORKERS = 1
-	NUM_JOBS    = 5
+	NUM_JOBS = 5
 )
 
 func workerFn(input string) string {
@@ -15,26 +15,10 @@ func workerFn(input string) string {
 	return fmt.Sprint(input, " - FINISHED")
 }
 
-func worker(jobs chan string) {
-	for job := range jobs {
-		output := workerFn(job)
-		fmt.Println(output)
-	}
-}
-
 func main() {
-	jobs := make(chan string, NUM_JOBS)
-
-	// Start your workers!
-	for i := 0; i < NUM_WORKERS; i++ {
-		go worker(jobs)
-	}
-
 	// Submit jobs
 	for i := 0; i < NUM_JOBS; i++ {
-		jobs <- fmt.Sprintf("Job #: %d", i)
+		output := workerFn(fmt.Sprintf("Job #: %d", i))
+		fmt.Println(output)
 	}
-
-	// Wait for everything to finish
-	time.Sleep(time.Second * ((NUM_JOBS / NUM_WORKERS) + 1))
 }
